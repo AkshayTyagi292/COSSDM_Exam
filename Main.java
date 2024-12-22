487271918b198943ca506513b386a6d05a6338bc
SN-20 <sonalnikam2003@gmail.com>
2024-12-22 17:20:55 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] var0) {
+        for(int var1 = 1; var1 <= 200; var1 += 2) {
+           System.out.print("" + var1 + " ");
+        }
+  
+     }
+}
